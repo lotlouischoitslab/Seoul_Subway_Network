56d5fcd9a7ad601fd9db117d1b8dea96f998bafc
lotlouischoitslab <louis.s.cho@gmail.com>
2023-07-09 15:44:54 -0500

    add

--- src/utils.py
+++ src/utils.py
@@ -21,7 +21,7 @@ class Graph: # Graph Class
             self.nodes.append(add_node) # we want to add the node
             self.graph[add_node] = [] # obviously initialize this
     
-    def add_edge(self,u,v,weight):
+    def add_edge(self,u,v,weight): # add an edge between the nodes
         u = tuple(u) # convert u into tuples
         v = tuple(v) # convert v into tuples
         if u in self.nodes and v in self.nodes: # if both are not in the nodes
@@ -75,39 +75,39 @@ class Graph: # Graph Class
             print(n,'-->',self.graph[n]) # print out the adjacency list
     
     def bfs(self,source): # BFS Algorithm
-        if len(self.graph) == 0: 
-            return []
+        if len(self.graph) == 0: # if the graph is empty
+            return [] # just return the empty list 
         
-        source = tuple(source)
-        to_traverse = [source]
-        queue = [source]
-        visited = set() 
+        source = tuple(source) # source node
+        to_traverse = [source] # add the source node to the traversed list 
+        queue = [source] # push the source to the queue
+        visited = set() # create an empty visited set
 
-        while len(queue) != 0:
-            v = queue.pop(0) 
-            visited.add(v) 
+        while len(queue) != 0: # while the queue is not empty
+            v = queue.pop(0) # pop the element from the queue
+            visited.add(v) # add the popped element into the visited set
 
-            for neighbor,weight in self.graph[v]:
-                if neighbor not in visited and neighbor not in queue:
-                    queue.append(neighbor) 
-                    to_traverse.append(neighbor)
+            for neighbor,weight in self.graph[v]: # for each neighbor and weight
+                if neighbor not in visited and neighbor not in queue: # if the neighbor is not visited and not in the queue
+                    queue.append(neighbor) # add the neighbor into the queue
+                    to_traverse.append(neighbor) # add the neighbor to the traversed list
         
-        return to_traverse
+        return to_traverse # return the traversed nodes
 
-    def dfs(self,source):
-        visited = set()
-        source = tuple(source)
-        return self.dfs_recursive(source,visited,[])
+    def dfs(self,source): # DFS
+        visited = set() # visited
+        source = tuple(source) # convert source to tuple
+        return self.dfs_recursive(source,visited,[]) # call the recursive dfs function
     
-    def dfs_recursive(self,source,visited,to_traverse):
-        visited.add(source) 
-        to_traverse.append(source) 
+    def dfs_recursive(self,source,visited,to_traverse): # helper function for dfs
+        visited.add(source) # add the visited set
+        to_traverse.append(source) # add the traverse list
 
-        for neighbor,weight in self.graph[source]:
-            if neighbor not in visited:
-                self.dfs_recursive(neighbor,visited,to_traverse)
+        for neighbor,weight in self.graph[source]: # for each neighbor and weight
+            if neighbor not in visited: # if the neighbor is not visited
+                self.dfs_recursive(neighbor,visited,to_traverse) # call the recursive function
         
-        return to_traverse
+        return to_traverse # return the to_traverse
 
     #A* Shortest Path Algorithm
     def heuristic(self,n):
@@ -116,60 +116,61 @@ class Graph: # Graph Class
             H_dist[n] = 0
         return 2
 
-    def astar(self, source, target):
-        source = tuple(source)
-        target = tuple(target)
-        open_list = set([source])
-        closed_list = set([])
+    def astar(self, source, target): # A* Shortest Paths
+        source = tuple(source) # convert source to tuple
+        target = tuple(target) # convert target to tuple
+        open_list = set([source]) # open list to store nodes to be explored
+        closed_list = set([]) # closed list to store visited nodes
 
-        g = {}
+        g = {} # dictionary to store the cost from the start node to each node
         g[source] = 0
 
-        distances = {}
+        distances = {} # dictionary to store the total estimated distance from the start node to each node
         distances[source] = 0
 
-        parents = {}
+        parents = {} # dictionary to store the parent node of each node in the shortest path
         parents[source] = source
 
-        while len(open_list) > 0:
-            n = None
-            for v in open_list:
-                if n is None or g[v] + self.heuristic(v) < g[n] + self.heuristic(n):
-                    n = v
-                    distances[n] = g[v] + self.heuristic(v)
+        while len(open_list) > 0: # begins a loop that continues until there are no more nodes left to explore in open_list
+            n = None # n is set None to represent the current node being examined
+            for v in open_list: # loop iterates over each node v in the open list
+                if n is None or g[v] + self.heuristic(v) < g[n] + self.heuristic(n): # it checks if n is None or the estimated total cost of reaching v is lower than the estimated cost
+                    n = v # set n to v
+                    distances[n] = g[v] + self.heuristic(v) # update the distance
 
-            if n is None:
-                return None  # No path exists
+            if n is None: # if n is None
+                return None  # No path exists and no more paths to explore and no path exists from source to target
 
-            if n == target:
-                path = []
+            if n == target: # if the current node n is equal to the target, this means that the shortest path has been found
+                path = [] # we start with empty path and iteratively add each node n and its corresponding path
 
-                while parents[n] != n:
-                    path.append([n, distances[n]])
-                    n = parents[n]
-                path.append([source, distances[source]])
-                path.reverse()
-                return path, distances[target]
+                while parents[n] != n: # continue to add path until reaching the source node
+                    path.append([n, distances[n]]) # add the path and distance
+                    n = parents[n] # n = parent
+                path.append([source, distances[source]]) # append the path
+                path.reverse() # reverse the path
+                return path, distances[target] # return the path and the distances
 
-            for neighbor, weight in self.graph[n]:
-                if neighbor not in open_list and neighbor not in closed_list:
-                    open_list.add(neighbor)
-                    parents[neighbor] = n
-                    g[neighbor] = g[n] + weight
+            for neighbor, weight in self.graph[n]: # for each neighbor node and weight in graph[n]
+                if neighbor not in open_list and neighbor not in closed_list: # for each neighbor, it checks if the neighbor is in open_list or closed_list
+                    open_list.add(neighbor) # if the neighbor is not in either of them, add to open list and its cost and parent information are updated
+                    parents[neighbor] = n # update the parent node
+                    g[neighbor] = g[n] + weight # update the weight
 
-                else:
-                    if g[neighbor] > g[n] + weight:
+                else: # If the neighbor is in the closed_list, it means it was visited before
+                    if g[neighbor] > g[n] + weight: # check if shorter path has been found
                         g[neighbor] = g[n] + weight
                         parents[neighbor] = n
 
-                        if neighbor in closed_list:
-                            closed_list.remove(neighbor)
-                            open_list.add(neighbor)
+                        if neighbor in closed_list: # if the neighbor is in the closed list, this means that it was visited before
+                            closed_list.remove(neighbor) # neighbor is removed from closed list
+                            open_list.add(neighbor) # neighbor is added on to the open list
 
-            open_list.remove(n)
-            closed_list.add(n)
+            open_list.remove(n) # once all the neighbors of n have been processed, n is removed from open list
+            closed_list.add(n) # then added to the closed list
 
         return None  # No path exists
+
 
     def dijkstra(self,source,target): 
         source = tuple(source)
